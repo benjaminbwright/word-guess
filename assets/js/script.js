ace0b164f0feed52eb206bbb80b17e2618424a8c
Benjamin Wright <bwright@trilogyed.com>
2022-05-16 14:36:45 -0500

    Fleshed out pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,75 @@
 // DEPENDENCIES =========================================
 // start button
 var startButton = document.querySelector("#start");
-// document
-
+// blanks container
+// reset button
+// wins element
+// losses element
 
 // STATE =========================================
-// wordBank 
 // the current word 
+// guessed word (may contain blanks)
 
 // FUNCTIONS =========================================
 // start game 
 function startGame() {
-  // timer reset
   // start the timer
-  // pick a word from the wordbank
-  // genarate an unguessed word
-  // render the unguessed word
+  // pick random word as the current word
+  // genarate an unguessed word (blanks)
+  
 }
-function startTimer() {
 
+
+function startTimer() {
+  console.log("starting the timer")
+  // set the starting time
+  // start the timer running every second
+    // if there's time left
+      // counting down on the timer
+      // render the updated time on th page
+      // checking for a win? true or false
+      // if it's a win
+        // you stop the timer
+        // you win
+    // otherwise (there's no time left)
+      // stop the timer
+      // you lose
+}
+
+function checkWin() {
+  console.log("checking for a win")
+  // return: does the guessed word match the current word ? true or false
+}
+
+function youLose() {
+  // record the loss 
+  // display a "you lose"
+}
+
+function youWin() {
+  // record the w 
+  // display a "you lose"
 }
 
 function pickRandomWord() {
-  
-}
-// guess letter
+  // keep the wordbank here
+  // get a random index
+  // get the word at that index from the wordbank
 
+}
+function generateGuessedWord() {
+  // turn the word into blanks return it
+  // render the guessed word
+}
+
+function renderGuessedWord() {
+  // put the guessed word on the page with blanks
+}
+
+function guessLetter(key) {
+  // check if the key is in the current word
+    // yes? go through the guessed word and replace the blanks with that letter where appropriate
+}
 
 // USER INTERACTIONS =========================================
 // a user clicks the start button
@@ -33,6 +77,7 @@ function pickRandomWord() {
 
 // a user presses a key
   // guess letter (someLetter)
+  // 
 
 // a user presses reset scores
 
