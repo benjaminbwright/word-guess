ed39b54a28cf2f6e790ccc1bab655de072898ef1
Benjamin Wright <bwright@trilogyed.com>
2022-05-16 13:47:01 -0500

    Added base files

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// DEPENDENCIES =========================================
+// start button
+var startButton = document.querySelector("#start");
+// document
+
+
+// STATE =========================================
+// wordBank 
+// the current word 
+
+// FUNCTIONS =========================================
+// start game 
+function startGame() {
+  // timer reset
+  // start the timer
+  // pick a word from the wordbank
+  // genarate an unguessed word
+  // render the unguessed word
+}
+function startTimer() {
+
+}
+
+function pickRandomWord() {
+  
+}
+// guess letter
+
+
+// USER INTERACTIONS =========================================
+// a user clicks the start button
+  // start game
+
+// a user presses a key
+  // guess letter (someLetter)
+
+// INITIALIZATION =========================================
